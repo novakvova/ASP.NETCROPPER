473a53b01ac8943a411dfbc230c6638f4ffbccca
novakvova <novakvova@gmail.com>
2019-02-09 16:50:32 +0200

    Cropper js

---
+++ WebSiteCrop/Models/ProducViewModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSiteCrop.Models
+{
+    public class ProducCreateViewModel
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+    }
+}
